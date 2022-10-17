0e7e14365e5e409ec9e332f689dc944ffe972f75
Aminulhoque1 <aminulkupa50@gmail.com>
2022-10-17 12:53:34 -0700

    friest commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASuUGwRkehuRcXyaonuMxfy9osT6u2ttg",
+  authDomain: "daragon-news.firebaseapp.com",
+  projectId: "daragon-news",
+  storageBucket: "daragon-news.appspot.com",
+  messagingSenderId: "196033198048",
+  appId: "1:196033198048:web:dc703fb50367d8b6cb5d85"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
